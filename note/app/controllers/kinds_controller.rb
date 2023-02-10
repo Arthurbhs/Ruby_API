bde5d70d9bb08d2f631411d9949c497495b75c8f
arthur <arthur.shekinarfoxy@gmail.com>
2023-02-10 12:25:18 -0300

    feat: http authentication

--- note/app/controllers/kinds_controller.rb
+++ note/app/controllers/kinds_controller.rb
@@ -1,4 +1,9 @@
 class KindsController < ApplicationController
+  
+  include ActionController::HttpAuthentication:: Digest::ControllerMethods
+  USERS = {"Arthur" => Digest::MD5.hexdigest(["Arthur", "Application", "secret"].join(":"))}
+   
+  before_action :authenticate
   before_action :set_kind, only: [:show, :update, :destroy]
 
   # GET /kinds
@@ -51,5 +56,11 @@ class KindsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def kind_params
       params.require(:kind).permit(:description)
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_digest(REALM) do |username|
+        USERS[username]
+      end
     end
 end
